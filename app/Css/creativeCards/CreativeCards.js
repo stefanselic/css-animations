b2660bec168d8426a0836163eb54c799e95ad349
stefanselic <s.stefan22@hotmail.com>
2024-03-19 13:41:22 +0100

    Add card effect

--- app/Css/creativeCards/CreativeCards.js
+++ app/Css/creativeCards/CreativeCards.js
@@ -4,7 +4,7 @@ import './CreativeCards.css';
 
 export default function CreativeCards() {
   return (
-    <main>
+    <main class="main-container">
       <div class="card-1">
         <div class="card-1-content">
           <h2>design</h2>
@@ -14,6 +14,20 @@ export default function CreativeCards() {
             incididunt ut labore et, incididunt ut labore et.
           </p>
           <a href="#">read more</a>
+        </div>
+      </div>
+      <br />
+      <div class="card-2">
+        <div class="imgbox">
+          <img src="2.jpg" />
+        </div>
+        <div class="card-2-content">
+          <h2>card hover</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam,
+          </p>
         </div>
       </div>
     </main>
