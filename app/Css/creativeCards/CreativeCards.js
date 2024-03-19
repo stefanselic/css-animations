b4aef92cda2d64e2fbca1ad7336539cfec83a691
stefanselic <s.stefan22@hotmail.com>
2024-03-19 22:31:20 +0100

    Add more card effects

--- app/Css/creativeCards/CreativeCards.js
+++ app/Css/creativeCards/CreativeCards.js
@@ -58,6 +58,94 @@ export default function CreativeCards() {
           <h2>88</h2>
         </div>
       </div>
+      <br />
+      <div class="card-5-container">
+        <div class="card-5">
+          <div class="card-5-face1">
+            <div class="imgbox">
+              <img src="3.png" />
+              <h3>design</h3>
+            </div>
+          </div>
+          <div class="card-5-face2">
+            <div class="card-5-content">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                cum cumque minus iste veritatis provident at.
+              </p>
+              <a href="#">read more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="card-6">
+        <div class="card-6-imgbox">
+          <img src="man.jpg" />
+        </div>
+        <div class="card-6-details">
+          <h2>
+            john doe
+            <span>founder</span>
+          </h2>
+        </div>
+      </div>
+      <br />
+      <div class="card-7">
+        <div class="card-7-imgbox">
+          <img src="1.jpg" />
+        </div>
+        <div class="card-7-content">
+          <h3>post title</h3>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamcon Ut enim ad minim
+            veniam, quis nostrud exercitation
+          </p>
+          <a href="#">read more</a>
+        </div>
+      </div>
+      <br />
+      <div class="card-8">
+        <div class="card-8-imgbox">
+          <img src="headphone1.png" />
+        </div>
+        <div class="card-8-content">
+          <h3>Wireless Headphones</h3>
+          <h2 class="price">
+            $40.<small>99</small>
+          </h2>
+          <a href="#" class="buy">
+            Buy Now
+          </a>
+        </div>
+      </div>
+      <br />
+      <div class="card-9-container">
+        <div class="card-9">
+          <div class="card-9-imgbox">
+            <img src="shoes.png" />
+          </div>
+          <div class="card-9-content">
+            <h2>Nike Shoes</h2>
+            <div class="size">
+              <h3>size:</h3>
+              <span>7</span>
+              <span>8</span>
+              <span>9</span>
+              <span>10</span>
+            </div>
+            <div class="color">
+              <h3>color:</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href="#">buy now</a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
